679d914a12f09af8b079e32c92e84f360ec1e1d7
mehdi rabani <152238535+mehdirabani@users.noreply.github.com>
2026-07-22 01:31:52 +0330

    chore: refresh asset version for v1.4.3

--- config/settings.php
+++ config/settings.php
@@ -3,7 +3,7 @@
 return [
     'app_name' => 'پروما',
     'version' => (require __DIR__ . '/version.php')['application'],
-    'asset_version' => '1.4.2-rc.1',
+    'asset_version' => '1.4.3-rc.1',
     'timezone' => 'Asia/Tehran',
     'base_url' => '',
     'session_name' => 'proma_pay_session',
